ae8ae258ff6b25f97016a09359c2b5e86afe059f
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2025-05-02 20:01:55 +0700

    edit effect

--- src/pages/Home/component/Title/HealthText/index.tsx
+++ src/pages/Home/component/Title/HealthText/index.tsx
@@ -14,7 +14,11 @@ const HealthText = () => {
   return (
     <div
       className={`flex justify-center items-center  transform transition-all duration-1000 
-      ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}
+      ${
+        isVisible
+          ? 'translate-y-0 opacity-100'
+          : 'translate-y-4 opacity-0 sm:translate-y-0 sm:translate-x-4 '
+      }`}
     >
       <h1 className='font-en text-[36px] font-bold bg-linear-to-b from-[#F2F2F2] from-44% to-[#8C8C8C] bg-clip-text text-transparent drop-shadow-[3px_4px_4px_black] sm:text-[48px]'>
         LIFE
--- src/pages/Home/component/Title/InsuranceText/index.tsx
+++ src/pages/Home/component/Title/InsuranceText/index.tsx
@@ -14,7 +14,11 @@ const InsuranceText = () => {
   return (
     <div
       className={`flex justify-center items-center  transform transition-all duration-1000 sm:gap-x-2 
-      ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}
+      ${
+        isVisible
+          ? 'translate-y-0 opacity-100'
+          : 'translate-y-4 opacity-0 sm:translate-y-0 sm:translate-x-4'
+      }`}
     >
       <Insurance />
       <h1 className='font-en text-[36px] font-bold bg-linear-to-b from-[#F2F2F2] from-44% to-[#8C8C8C] bg-clip-text text-transparent drop-shadow-[3px_4px_4px_black] sm:text-[48px]'>
